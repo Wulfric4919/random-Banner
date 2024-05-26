3ce502fdf60264b6f950197f05e683f5e0c166e2
Wulfric4919 <0620jyun@gmail.com>
2024-05-26 23:57:00 +0900

    Update myapp.sh

--- myapp.sh
+++ myapp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# myapp.sh
+# start.sh
 
-# プロジェクトディレクトリに移動
-cd /Wulfric4919/random-Banner/tree/main
+# 必要な場合、仮想環境をアクティブ化
+# source venv/bin/activate
 
 # Gunicornを起動
 gunicorn --bind 0.0.0.0:8000 app:app
 chmod +x myapp.sh
-./myapp.sh
