1359e681dfee5a02531484ee746738bc2dfbd481
Wulfric4919 <0620jyun@gmail.com>
2024-05-30 15:42:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def root_text():
     "Root page"
     return ("No contents in this page.")
 
-@app.route("/banner.png", methods=['GET'])
+@app.route("/banne.png", methods=['GET'])
 def get_banner():
     "Returns random banner"
     banners = glob("./static/banners/*.png")
