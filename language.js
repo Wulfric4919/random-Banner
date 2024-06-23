a9dbd26cfa360a3c4f724048cb87a52f7ec2a8df
Wulfric4919 <0620jyun@gmail.com>
2024-06-23 13:47:48 +0900

    Create language.js

---
+++ language.js
@@ -0,0 +1,12 @@
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<!--<script type="text/javascript">
+	function gtElInit(){
+	var lib = new google.translate.TranslateService();lib.translatePage('ja', 'en', function () {});
+}
+</script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'ja', 
+    layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT
+  }, 'google_translate_element');
+}
